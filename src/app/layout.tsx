2caee9a2a984cccbb0a00e68f94ebed17f5be7c6
Silvester Kresna W. P. P. <18221049@std.stei.itb.ac.id>
2024-02-27 00:58:35 +0700

    membuat design baru

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,6 +18,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Reggae+One&display=swap" rel="stylesheet" />
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Reggae+One&family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet" />
+      </head>
       <body className={inter.className}>
         <Navbar />
         <div className="container">{children}</div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,9 +3,12 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <div className="flex justify-between bg-blue-200 py-2 px-5">
+    <div className="flex mb-4 justify-between font-mono  py-2 px-5">
       <Link href="/">
         <h1 className="font-bold">Classic Cryptography</h1>
+      </Link>
+      <Link href="/design-baru">
+        <div className="hover:font-semibold">Design Baru</div>
       </Link>
       <ul className="flex space-x-6 font-medium">
         <Link href="/vigenere-cipher">
